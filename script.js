b65a2d6cf6d8a8987f0a6a5672230b9e0b4a0950
aaliyaahd <aliyahdaimah@gmail.com>
2026-05-26 18:33:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,700 @@
+const menus = [
+
+  {
+    minggu: 1,
+
+    hariTanggal: "Senin, 06 April 2026",
+
+    menu:
+    "Nasi Putih + Ayam Crispy Saos Mentai + Tahu Asam Manis + Capcay + Pisang",
+
+    hargaKecil: 8000,
+    hargaBesar: 10000,
+
+    giziKecil: 88,
+    giziBesar: 95
+  },
+
+  {
+    minggu: 1,
+
+    hariTanggal: "Selasa, 07 April 2026",
+
+    menu:
+    "Nasi Putih + Ayam Kecap + Telur Scramble + Tempe Goreng + Sayur Sop + Kelengkeng",
+
+    hargaKecil: 8000,
+    hargaBesar: 10000,
+
+    giziKecil: 90,
+    giziBesar: 96
+  },
+
+  {
+    minggu: 1,
+
+    hariTanggal: "Rabu, 08 April 2026",
+
+    menu:
+    "Nasi Daun Pandan + Telur Ceplok + Tahu Crispy + Tumis Wortel + Apel Fuji",
+
+    hargaKecil: 8000,
+    hargaBesar: 10000,
+
+    giziKecil: 85,
+    giziBesar: 92
+  },
+
+  {
+    minggu: 1,
+
+    hariTanggal: "Kamis, 09 April 2026",
+
+    menu:
+    "Nasi Putih + Ayam Goreng + Orek Tempe + Tumis Labusiam + Pisang",
+
+    hargaKecil: 8000,
+    hargaBesar: 10000,
+
+    giziKecil: 87,
+    giziBesar: 94
+  },
+
+  {
+    minggu: 1,
+
+    hariTanggal: "Jumat, 10 April 2026",
+
+    menu:
+    "Nasi Putih + Telur Ceplok + Tempe Mendoan + Buncis + Jeruk Kino",
+
+    hargaKecil: 8000,
+    hargaBesar: 10000,
+
+    giziKecil: 84,
+    giziBesar: 91
+  },
+  {
+  minggu: 1,
+
+  hariTanggal: "Selasa, 31 Maret 2026",
+
+  menu:
+  "Nasi Putih + Ayam Katsu + Telur Scramble + Tahu Bali + Tumis Kubis + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 89,
+  giziBesar: 95
+},
+
+{
+  minggu: 1,
+
+  hariTanggal: "Rabu, 01 April 2026",
+
+  menu:
+  "Nasi Daun Jeruk + Ayam Woku + Tahu Goreng + Tumis Wortel + Jeruk Medan",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 88,
+  giziBesar: 94
+},
+
+{
+  minggu: 1,
+
+  hariTanggal: "Kamis, 02 April 2026",
+
+  menu:
+  "Nasi Putih + Telur Ceplok Balado + Ayam Suwir + Tahu Goreng + Tumis Buncis + Kelengkeng",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 90,
+  giziBesar: 96
+},
+
+{
+  minggu: 1,
+
+  hariTanggal: "Jumat, 03 April 2026",
+
+  menu:
+  "Nasi Putih + Ayam Teriyaki + Orek Tempe + Pakcoy Siram + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 87,
+  giziBesar: 93
+},
+
+{
+  minggu: 1,
+
+  hariTanggal: "Sabtu, 04 April 2026",
+
+  menu:
+  "Nasi Putih + Telur Ceplok Asam Manis + Stik Tempe + Mix Vege + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 86,
+  giziBesar: 92
+},
+
+{
+  minggu: 2,
+
+  hariTanggal: "Senin, 06 April 2026",
+
+  menu:
+  "Nasi Putih + Ayam Crispy Saos Mentai + Tahu Asam Manis + Capcay + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 88,
+  giziBesar: 95
+},
+
+{
+  minggu: 2,
+
+  hariTanggal: "Selasa, 07 April 2026",
+
+  menu:
+  "Nasi Putih + Ayam Kecap + Telur Scramble + Tempe Goreng + Sayur Sop + Kelengkeng",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 90,
+  giziBesar: 96
+},
+
+{
+  minggu: 2,
+
+  hariTanggal: "Rabu, 08 April 2026",
+
+  menu:
+  "Nasi Daun Pandan + Telur Ceplok + Tahu Crispy + Tumis Wortel + Apel Fuji",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 85,
+  giziBesar: 92
+},
+
+{
+  minggu: 2,
+
+  hariTanggal: "Kamis, 09 April 2026",
+
+  menu:
+  "Nasi Putih + Ayam Goreng Bawang Putih + Orek Tempe + Tumis Labusiam + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 87,
+  giziBesar: 94
+},
+
+{
+  minggu: 2,
+
+  hariTanggal: "Jumat, 10 April 2026",
+
+  menu:
+  "Nasi Putih + Telur Ceplok Bumbu Kecap + Tempe Mendoan + Buncis + Jeruk Kino",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 84,
+  giziBesar: 91
+},
+
+{
+  minggu: 2,
+
+  hariTanggal: "Sabtu, 11 April 2026",
+
+  menu:
+  "Nasi Putih + Ayam Goreng Lengkuas + Tahu Goreng + Sayur Asem + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 89,
+  giziBesar: 95
+},
+
+{
+  minggu: 3,
+
+  hariTanggal: "Senin, 13 April 2026",
+
+  menu:
+  "Nasi Putih + Ayam Fillet Asam Manis + Stik Tempe + Buncis + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 89,
+  giziBesar: 95
+},
+
+{
+  minggu: 3,
+
+  hariTanggal: "Selasa, 14 April 2026",
+
+  menu:
+  "Nasi Putih + Telur Balado + Tahu Goreng + Tumis Wortel + Jeruk Kino",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 86,
+  giziBesar: 92
+},
+
+{
+  minggu: 3,
+
+  hariTanggal: "Rabu, 15 April 2026",
+
+  menu:
+  "Nasi Putih + Sate Yakitori + Tempe Mendoan + Salad Sayur + Kelengkeng",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 90,
+  giziBesar: 96
+},
+
+{
+  minggu: 3,
+
+  hariTanggal: "Kamis, 16 April 2026",
+
+  menu:
+  "Nasi Putih + Ayam Popcorn + Tahu Kecap + Capcay + Anggur Muscat",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 88,
+  giziBesar: 94
+},
+
+{
+  minggu: 3,
+
+  hariTanggal: "Jumat, 17 April 2026",
+
+  menu:
+  "Roti Tawar + Keju Slice + Ayam Katsu + Patty Tempe + Buah Naga",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 91,
+  giziBesar: 97
+},
+
+{
+  minggu: 4,
+
+  hariTanggal: "Senin, 20 April 2026",
+
+  menu:
+  "Nasi Putih + Ayam Bakar Madu + Tahu + Tumis Brokoli + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 89,
+  giziBesar: 95
+},
+
+{
+  minggu: 4,
+
+  hariTanggal: "Selasa, 21 April 2026",
+
+  menu:
+  "Nasi Putih + Telur Fuyunghai + Tempe Lilit + Tumis Wortel + Kelengkeng",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 88,
+  giziBesar: 94
+},
+
+{
+  minggu: 4,
+
+  hariTanggal: "Rabu, 22 April 2026",
+
+  menu:
+  "Nasi Daun Jeruk + Ayam Serundeng + Tahu Goreng + Tumis Buncis + Kelengkeng",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 90,
+  giziBesar: 96
+},
+
+{
+  minggu: 4,
+
+  hariTanggal: "Kamis, 23 April 2026",
+
+  menu:
+  "Nasi Putih + Asam-Asam Ayam + Tempe Mendoan + Tumis Labusiam + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 87,
+  giziBesar: 93
+},
+
+{
+  minggu: 4,
+
+  hariTanggal: "Jumat, 24 April 2026",
+
+  menu:
+  "French Fries + Chicken Steak + Tahu Crispy + Mixed Vege + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 86,
+  giziBesar: 91
+},
+
+{
+  minggu: 5,
+
+  hariTanggal: "Senin, 27 April 2026",
+
+  menu:
+  "Nasi Putih + Ayam Crispy + Sate Tempe + Tumis Sawi + Apel",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 89,
+  giziBesar: 95
+},
+
+{
+  minggu: 5,
+
+  hariTanggal: "Selasa, 28 April 2026",
+
+  menu:
+  "Nasi Putih + Dori Crispy + Tahu Bumbu Rujak + Acar Kuning + Jeruk Sunshine",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 90,
+  giziBesar: 96
+},
+
+{
+  minggu: 5,
+
+  hariTanggal: "Rabu, 29 April 2026",
+
+  menu:
+  "Nasi Putih + Ayam Kungpao + Sambal Goreng Tempe + Tumis Sawi + Kelengkeng",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 91,
+  giziBesar: 97
+},
+
+{
+  minggu: 5,
+
+  hariTanggal: "Kamis, 30 April 2026",
+
+  menu:
+  "Jagung Manis + Ayam Katsu + Stik Tempe + Mixed Vege + Pisang",
+
+  hargaKecil: 8000,
+  hargaBesar: 10000,
+
+  giziKecil: 89,
+  giziBesar: 95
+}
+
+];
+const menuTable =
+document.getElementById("menuTable");
+
+const variabelTable =
+document.getElementById("variabelTable");
+
+
+function tampilkanTabel(){
+
+  menuTable.innerHTML = "";
+
+  menus.forEach(item=>{
+
+    const row = `
+      <tr>
+        <td>${item.minggu}</td>
+        <td>${item.hariTanggal}</td>
+        <td>${item.menu}</td>
+        <td>Rp ${item.hargaKecil.toLocaleString()}</td>
+        <td>Rp ${item.hargaBesar.toLocaleString()}</td>
+        <td>${item.giziKecil}</td>
+        <td>${item.giziBesar}</td>
+      </tr>
+    `;
+
+    menuTable.innerHTML += row;
+  });
+}
+
+
+
+function tampilkanVariabel(){
+
+  variabelTable.innerHTML = "";
+
+  menus.forEach((item,index)=>{
+
+    let variabel =
+    "X" + (index+1);
+
+    const row = `
+      <tr>
+        <td>${variabel}</td>
+        <td>
+        Menu makanan
+        ${item.hariTanggal}
+        Minggu ke-${item.minggu}
+        </td>
+        <td>${item.menu}</td>
+      </tr>
+    `;
+
+    variabelTable.innerHTML += row;
+  });
+}
+
+
+
+function optimasiMenu(){
+
+  let kategori =
+  document.getElementById("kategori").value;
+
+  let sortedMenus =
+  [...menus];
+
+
+  if(kategori==="kecil"){
+
+    sortedMenus.forEach(item=>{
+      item.rasio =
+      item.giziKecil /
+      item.hargaKecil;
+    });
+
+  } else {
+
+    sortedMenus.forEach(item=>{
+      item.rasio =
+      item.giziBesar /
+      item.hargaBesar;
+    });
+
+  }
+
+
+  // descending
+  sortedMenus.sort((a,b)=>
+    b.rasio-a.rasio
+  );
+
+
+  let hasil =
+  `<h3>Daftar Menu Optimal</h3><ul>`;
+
+
+  sortedMenus.forEach(item=>{
+
+    if(kategori==="kecil"){
+
+      hasil += `
+      <li>
+      <strong>${item.hariTanggal}</strong><br>
+      Variabel:
+      X${menus.indexOf(item)+1}
+      (Minggu ke-${item.minggu})<br>
+      🍽️ ${item.menu}<br>
+      👶 Balita / TK / SD 1-3<br>
+      📦 Porsi: Kecil<br>
+      💰 Rp ${item.hargaKecil.toLocaleString()}<br>
+      🥗 Gizi: ${item.giziKecil}<br>
+      📈 Rasio:
+      ${item.rasio.toFixed(4)}
+      <hr>
+      </li>
+      `;
+
+    } else {
+
+      hasil += `
+      <li>
+      <strong>${item.hariTanggal}</strong><br>
+      Variabel:
+      X${menus.indexOf(item)+1}
+      (Minggu ke-${item.minggu})<br>
+      🍽️ ${item.menu}<br>
+      👨‍👩‍👧 SD 4-6 / SMP / SMA /
+      Busui / Ibu Hamil / Lansia<br>
+      📦 Porsi: Besar<br>
+      💰 Rp ${item.hargaBesar.toLocaleString()}<br>
+      🥗 Gizi: ${item.giziBesar}<br>
+      📈 Rasio:
+      ${item.rasio.toFixed(4)}
+      <hr>
+      </li>
+      `;
+    }
+
+  });
+
+
+  hasil += "</ul>";
+
+  document.getElementById("hasil")
+  .innerHTML = hasil;
+
+}
+
+
+
+tampilkanTabel();
+tampilkanVariabel();
+
+const menuTable =
+document.getElementById("menuTable");
+
+menus.forEach(item => {
+
+  const row = `
+    <tr>
+
+      <td>${item.minggu}</td>
+
+      <td>${item.hariTanggal}</td>
+
+      <td>${item.menu}</td>
+
+      <td>
+        Rp ${item.hargaKecil.toLocaleString()}
+      </td>
+
+      <td>
+        Rp ${item.hargaBesar.toLocaleString()}
+      </td>
+
+      <td>${item.giziKecil}</td>
+
+      <td>${item.giziBesar}</td>
+
+    </tr>
+  `;
+
+  menuTable.innerHTML += row;
+
+});
+
+function optimasiMenu() {
+
+  let kategori =
+    document.getElementById("kategori").value;
+
+  let hasil =
+  `<h3>Daftar Menu Optimal</h3><ul>`;
+
+  menus.forEach(item => {
+
+    if(kategori==="kecil"){
+
+      let rasio =
+      item.giziKecil/item.hargaKecil;
+
+      hasil += `
+      <li>
+      <strong>${item.hariTanggal}</strong><br>
+      🍽️ ${item.menu}<br>
+      👶 Kategori:
+      Balita / TK / SD Kelas 1–3<br>
+      📦 Porsi:
+      Kecil<br>
+      💰 Harga:
+      Rp ${item.hargaKecil.toLocaleString()}<br>
+      🥗 Nilai Gizi:
+      ${item.giziKecil}<br>
+      📈 Rasio:
+      ${rasio.toFixed(4)}
+      </li>`;
+    }
+
+    else{
+
+      let rasio =
+      item.giziBesar/item.hargaBesar;
+
+      hasil += `
+      <li>
+      <strong>${item.hariTanggal}</strong><br>
+      🍽️ ${item.menu}<br>
+      👨‍👩‍👧 Kategori:
+      SD Kelas 4–6 / SMP / SMA /
+      Busui / Ibu Hamil / Lansia<br>
+      📦 Porsi:
+      Besar<br>
+      💰 Harga:
+      Rp ${item.hargaBesar.toLocaleString()}<br>
+      🥗 Nilai Gizi:
+      ${item.giziBesar}<br>
+      📈 Rasio:
+      ${rasio.toFixed(4)}
+      </li>`;
+    }
+
+  });
+
+  hasil += "</ul>";
+
+  document.getElementById("hasil")
+  .innerHTML = hasil;
+}
