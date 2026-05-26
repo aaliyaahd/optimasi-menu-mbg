1343632308d1a7d220819d0743dc0a6f0d7e77c6
aaliyaahd <aliyahdaimah@gmail.com>
2026-05-26 19:45:28 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const menus = [
   {
     minggu: 1,
-    hariTanggal: "Selasa, 31 Maret 2026",
-    menu: "Nasi Putih + Ayam Katsu + Telur Scramble + Tahu Bali + Tumis Kubis + Pisang",
+    hariTanggal: "Rabu, 1 April 2026",
+    menu: "Nasi Daun Jeruk + Ayam Woku + Tahu Goreng + Tumis Wortel Sawi Putih + Buah Jeruk Medan",
     hargaKecil: 8000,
     hargaBesar: 10000,
-    giziKecil: 89,
-    giziBesar: 95
-  },
-
-  {
-    minggu: 1,
-    hariTanggal: "Rabu, 01 April 2026",
-    menu: "Nasi Daun Jeruk + Ayam Woku + Tahu Goreng + Tumis Wortel + Jeruk Medan",
-    hargaKecil: 8000,
-    hargaBesar: 10000,
-    giziKecil: 88,
-    giziBesar: 94
+    giziKecil: 535,
+    giziBesar: 680
   },
 
   {
     minggu: 1,
     hariTanggal: "Kamis, 02 April 2026",
-    menu: "Nasi Putih + Telur Ceplok Balado + Ayam Suwir + Tahu Goreng + Tumis Buncis + Kelengkeng",
+    menu: "Nasi Putih + Telur Ceplok Balado + Ayam Suwir + Tahu Goreng + Tumis Buncis + Buah Kelengkeng",
     hargaKecil: 8000,
     hargaBesar: 10000,
-    giziKecil: 90,
-    giziBesar: 96
+    giziKecil: 540,
+    giziBesar: 720
   },
 
   {
     minggu: 1,
     hariTanggal: "Jumat, 03 April 2026",
-    menu: "Nasi Putih + Ayam Teriyaki + Orek Tempe + Pakcoy Siram + Pisang",
+    menu: "Nasi Putih + Ayam Teriyaki + Orek Tempe + Pakcoy Siram + Buah Pisang",
+    hargaKecil: 8000,
+    hargaBesar: 10000,
+    giziKecil: 545,
+    giziBesar: 690
+  },
+
+  {
+    minggu: 1,
+    hariTanggal: "Sabtu, 04 April 2026",
+    menu: "Nasi Putih + Telur Ceplok Asam Manis + Stik Tempe + Mix Vege + Buah Pisang",
     hargaKecil: 8000,
     hargaBesar: 10000,
     giziKecil: 87,
@@ -97,18 +97,69 @@ const menus = [
     hargaBesar: 10000,
     giziKecil: 84,
     giziBesar: 91
-  }
+  },
+
+
 ];
 
 const menuTable = document.getElementById("menuTable");
 const variabelTable = document.getElementById("variabelTable");
 
-function tampilkanTabel() {
+const mappingVariabel = {
+  "Senin-1": "X1",
+  "Senin-2": "X2",
+  "Senin-3": "X3",
+  "Senin-4": "X4",
+
+  "Selasa-1": "X5",
+  "Selasa-2": "X6",
+  "Selasa-3": "X7",
+  "Selasa-4": "X8",
+
+  "Rabu-1": "X9",
+  "Rabu-2": "X10",
+  "Rabu-3": "X11",
+  "Rabu-4": "X12",
+
+  "Kamis-1": "X13",
+  "Kamis-2": "X14",
+  "Kamis-3": "X15",
+  "Kamis-4": "X16",
+  "Kamis-5": "X17",
+
+  "Jumat-1": "X18",
+  "Jumat-2": "X19",
+  "Jumat-3": "X20",
+  "Jumat-4": "X21",
+
+  "Sabtu-1": "X22",
+  "Sabtu-2": "X23"
+};
+
+
+function ambilHari(namaHari){
+  if(namaHari.includes("Senin")) return "Senin";
+  if(namaHari.includes("Selasa")) return "Selasa";
+  if(namaHari.includes("Rabu")) return "Rabu";
+  if(namaHari.includes("Kamis")) return "Kamis";
+  if(namaHari.includes("Jumat")) return "Jumat";
+  if(namaHari.includes("Sabtu")) return "Sabtu";
+}
+
+
+function cariVariabel(item){
+  let hari = ambilHari(item.hariTanggal);
+  return mappingVariabel[`${hari}-${item.minggu}`] || "-";
+}
+
+
+function tampilkanTabel(){
   menuTable.innerHTML = "";
 
-  menus.forEach(item => {
+  menus.forEach(item=>{
     menuTable.innerHTML += `
       <tr>
+        <td>${cariVariabel(item)}</td>
         <td>${item.minggu}</td>
         <td>${item.hariTanggal}</td>
         <td>${item.menu}</td>
@@ -121,72 +172,60 @@ function tampilkanTabel() {
   });
 }
 
-function tampilkanVariabel() {
+
+function tampilkanVariabel(){
   variabelTable.innerHTML = "";
 
-  menus.forEach((item, index) => {
+  menus.forEach(item=>{
     variabelTable.innerHTML += `
       <tr>
-        <td>X${index + 1}</td>
-        <td>Menu Minggu ke-${item.minggu}</td>
+        <td>${cariVariabel(item)}</td>
+        <td>${item.hariTanggal}</td>
         <td>${item.menu}</td>
       </tr>
     `;
   });
 }
 
-function optimasiMenu() {
-  let kategori = document.getElementById("kategori").value;
+
+function optimasiMenu(){
+
+  let kategori =
+  document.getElementById("kategori").value;
+
   let sortedMenus = [...menus];
 
-  if (kategori === "kecil") {
-    sortedMenus.forEach(item => {
-      item.rasio = item.giziKecil / item.hargaKecil;
+  if(kategori==="kecil"){
+    sortedMenus.forEach(item=>{
+      item.rasio = item.giziKecil/item.hargaKecil;
     });
   } else {
-    sortedMenus.forEach(item => {
-      item.rasio = item.giziBesar / item.hargaBesar;
+    sortedMenus.forEach(item=>{
+      item.rasio = item.giziBesar/item.hargaBesar;
     });
   }
 
-  // descending
-  sortedMenus.sort((a, b) => b.rasio - a.rasio);
+  sortedMenus.sort((a,b)=>b.rasio-a.rasio);
 
   let hasil = "<h3>Daftar Menu Optimal</h3><ul>";
 
-  sortedMenus.forEach(item => {
-    if (kategori === "kecil") {
-      hasil += `
-        <li>
-          <b>${item.hariTanggal}</b><br>
-          Variabel: X${menus.indexOf(item) + 1}<br>
-          🍽️ ${item.menu}<br>
-          👶 Balita / TK / SD 1-3<br>
-          📦 Porsi Kecil<br>
-          💰 Rp ${item.hargaKecil.toLocaleString()}<br>
-          🥗 ${item.giziKecil}<br>
-          📈 ${item.rasio.toFixed(4)}
-          <hr>
-        </li>
-      `;
-    } else {
-      hasil += `
-        <li>
-          <b>${item.hariTanggal}</b><br>
-          Variabel: X${menus.indexOf(item) + 1}<br>
-          🍽️ ${item.menu}<br>
-          👨‍👩‍👧 SD 4-6 / SMP / SMA / Busui / Ibu Hamil / Lansia<br>
-          📦 Porsi Besar<br>
-          💰 Rp ${item.hargaBesar.toLocaleString()}<br>
-          🥗 ${item.giziBesar}<br>
-          📈 ${item.rasio.toFixed(4)}
-          <hr>
-        </li>
-      `;
-    }
+  sortedMenus.forEach(item=>{
+
+    let variabel = cariVariabel(item);
+
+    hasil += `
+      <li>
+      <b>${variabel}</b><br>
+      ${item.hariTanggal}<br>
+      🍽️ ${item.menu}<br>
+      📈 Rasio: ${item.rasio.toFixed(4)}
+      <hr>
+      </li>
+    `;
   });
 
   hasil += "</ul>";
+
   document.getElementById("hasil").innerHTML = hasil;
 }
 
