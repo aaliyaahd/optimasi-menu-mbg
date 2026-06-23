263b13a8182c83863fa941215ea4644381ce7c7d
aaliyaahd <aliyahdaimah@gmail.com>
2026-06-23 19:31:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const menus = [
 minggu:1,
 hariTanggal:"Rabu, 01 April 2026",
 menu:"Nasi Daun Jeruk + Ayam Woku + Tahu Goreng + Tumis Wortel + Jeruk Medan",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7500,
+hargaBesar:9200,
 giziKecil:530,
 giziBesar:710
 },
@@ -16,7 +16,7 @@ minggu:1,
 hariTanggal:"Kamis, 02 April 2026",
 menu:"Nasi Putih + Telur Ceplok Balado + Ayam Suwir + Tahu Goreng + Tumis Buncis + Buah Kelengkeng",
 hargaKecil:8000,
-hargaBesar:10000,
+hargaBesar:9800,
 giziKecil:540,
 giziBesar:720
 },
@@ -25,8 +25,8 @@ giziBesar:720
 minggu:1,
 hariTanggal:"Jumat, 03 April 2026",
 menu:"Nasi Putih + Ayam Teriyaki + Orek Tempe + Pakcoy Siram + Pisang",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7000,
+hargaBesar:8700,
 giziKecil:520,
 giziBesar:700
 },
@@ -35,8 +35,8 @@ giziBesar:700
 minggu:1,
 hariTanggal:"Sabtu, 04 April 2026",
 menu:"Nasi Putih + Telur Ceplok Asam Manis + Stik Tempe + Mix Vege + Pisang",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:8500,
+hargaBesar:10500,
 giziKecil:510,
 giziBesar:690
 },
@@ -45,8 +45,8 @@ giziBesar:690
 minggu:2,
 hariTanggal:"Senin, 06 April 2026",
 menu:"Nasi Putih + Ayam Crispy Saos Mentai + Tahu Asam Manis + Capcay + Pisang",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7800,
+hargaBesar:9600,
 giziKecil:550,
 giziBesar:730
 },
@@ -55,8 +55,8 @@ giziBesar:730
 minggu:2,
 hariTanggal:"Selasa, 07 April 2026",
 menu:"Nasi Putih + Ayam Kecap + Telur Scramble + Tempe Goreng + Sayur Sop + Kelengkeng",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:8200,
+hargaBesar:10200,
 giziKecil:560,
 giziBesar:740
 },
@@ -65,8 +65,8 @@ giziBesar:740
 minggu:2,
 hariTanggal:"Rabu, 08 April 2026",
 menu:"Nasi Daun Pandan + Telur Ceplok + Tahu Crispy + Tumis Wortel + Apel Fuji",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7600,
+hargaBesar:9400,
 giziKecil:500,
 giziBesar:680
 },
@@ -76,7 +76,7 @@ minggu:2,
 hariTanggal:"Kamis, 09 April 2026",
 menu:"Nasi Putih + Ayam Goreng + Orek Tempe + Tumis Labusiam + Pisang",
 hargaKecil:8000,
-hargaBesar:10000,
+hargaBesar:9900,
 giziKecil:530,
 giziBesar:710
 },
@@ -85,8 +85,8 @@ giziBesar:710
 minggu:2,
 hariTanggal:"Jumat, 10 April 2026",
 menu:"Nasi Putih + Telur Ceplok + Tempe Mendoan + Buncis + Jeruk Kino",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7300,
+hargaBesar:8900,
 giziKecil:490,
 giziBesar:670
 },
@@ -95,8 +95,8 @@ giziBesar:670
 minggu:2,
 hariTanggal:"Sabtu, 11 April 2026",
 menu:"Nasi Putih + Ayam Goreng Lengkuas + Tahu Goreng + Sayur Asem + Pisang",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:8700,
+hargaBesar:11000,
 giziKecil:540,
 giziBesar:720
 },
@@ -105,8 +105,8 @@ giziBesar:720
 minggu:3,
 hariTanggal:"Senin, 13 April 2026",
 menu:"Nasi Putih + Ayam Fillet Asam Manis + Stik Tempe + Buncis + Pisang",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7900,
+hargaBesar:9700,
 giziKecil:550,
 giziBesar:730
 },
@@ -115,8 +115,8 @@ giziBesar:730
 minggu:3,
 hariTanggal:"Selasa, 14 April 2026",
 menu:"Nasi Putih + Telur Balado + Tahu Goreng + Tumis Wortel + Jeruk Kino",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:8100,
+hargaBesar:10100,
 giziKecil:500,
 giziBesar:680
 },
@@ -125,8 +125,8 @@ giziBesar:680
 minggu:3,
 hariTanggal:"Rabu, 15 April 2026",
 menu:"Nasi Putih + Sate Yakitori + Tempe Mendoan + Salad Sayur + Kelengkeng",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7400,
+hargaBesar:8800,
 giziKecil:560,
 giziBesar:740
 },
@@ -135,8 +135,8 @@ giziBesar:740
 minggu:3,
 hariTanggal:"Kamis, 16 April 2026",
 menu:"Nasi Putih + Ayam Popcorn + Tahu Kecap + Capcay + Anggur Muscat",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:8300,
+hargaBesar:10300,
 giziKecil:540,
 giziBesar:720
 },
@@ -145,8 +145,8 @@ giziBesar:720
 minggu:3,
 hariTanggal:"Jumat, 17 April 2026",
 menu:"Roti Tawar + Keju Slice + Ayam Katsu + Patty Tempe + Buah Naga",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7700,
+hargaBesar:9500,
 giziKecil:570,
 giziBesar:750
 },
@@ -155,8 +155,8 @@ giziBesar:750
 minggu:4,
 hariTanggal:"Senin, 20 April 2026",
 menu:"Nasi Putih + Ayam Bakar Madu + Tahu + Tumis Brokoli + Pisang",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:8400,
+hargaBesar:10700,
 giziKecil:550,
 giziBesar:730
 },
@@ -165,8 +165,8 @@ giziBesar:730
 minggu:4,
 hariTanggal:"Selasa, 21 April 2026",
 menu:"Nasi Putih + Telur Fuyunghai + Tempe Lilit + Tumis Wortel + Kelengkeng",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7200,
+hargaBesar:8600,
 giziKecil:540,
 giziBesar:720
 },
@@ -176,7 +176,7 @@ minggu:4,
 hariTanggal:"Rabu, 22 April 2026",
 menu:"Nasi Daun Jeruk + Ayam Serundeng + Tahu Goreng + Tumis Buncis + Kelengkeng",
 hargaKecil:8000,
-hargaBesar:10000,
+hargaBesar:9900,
 giziKecil:560,
 giziBesar:740
 },
@@ -185,8 +185,8 @@ giziBesar:740
 minggu:4,
 hariTanggal:"Kamis, 23 April 2026",
 menu:"Nasi Putih + Asam-Asam Ayam + Tempe Mendoan + Tumis Labusiam + Pisang",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7500,
+hargaBesar:9300,
 giziKecil:530,
 giziBesar:710
 },
@@ -195,8 +195,8 @@ giziBesar:710
 minggu:4,
 hariTanggal:"Jumat, 24 April 2026",
 menu:"French Fries + Chicken Steak + Tahu Crispy + Mixed Vege + Pisang",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:8600,
+hargaBesar:10800,
 giziKecil:500,
 giziBesar:680
 },
@@ -205,8 +205,8 @@ giziBesar:680
 minggu:5,
 hariTanggal:"Senin, 27 April 2026",
 menu:"Nasi Putih + Ayam Crispy + Sate Tempe + Tumis Sawi + Apel",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7800,
+hargaBesar:9600,
 giziKecil:550,
 giziBesar:730
 },
@@ -215,8 +215,8 @@ giziBesar:730
 minggu:5,
 hariTanggal:"Selasa, 28 April 2026",
 menu:"Nasi Putih + Dori Crispy + Tahu Bumbu Rujak + Acar Kuning + Jeruk Sunshine",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:8200,
+hargaBesar:10400,
 giziKecil:560,
 giziBesar:740
 },
@@ -225,8 +225,8 @@ giziBesar:740
 minggu:5,
 hariTanggal:"Kamis, 30 April 2026",
 menu:"Jagung Manis + Ayam Katsu + Stik Tempe + Mixed Vege + Pisang",
-hargaKecil:8000,
-hargaBesar:10000,
+hargaKecil:7100,
+hargaBesar:8500,
 giziKecil:550,
 giziBesar:730
 }
@@ -289,6 +289,58 @@ const giziBesarMap = {
   X22:690, X23:650
 };
 
+const hargaKecilMap = {
+X1:7500,
+X2:8000,
+X3:7000,
+X4:8500,
+X5:7800,
+X6:8200,
+X7:7600,
+X8:8000,
+X9:7300,
+X10:8700,
+X11:7900,
+X12:8100,
+X13:7400,
+X14:8300,
+X15:7700,
+X16:8400,
+X17:7200,
+X18:8000,
+X19:7500,
+X20:8600,
+X21:7800,
+X22:8200,
+X23:7100
+};
+
+const hargaBesarMap = {
+X1:9200,
+X2:9800,
+X3:8700,
+X4:10500,
+X5:9600,
+X6:10200,
+X7:9400,
+X8:9900,
+X9:8900,
+X10:11000,
+X11:9700,
+X12:10100,
+X13:8800,
+X14:10300,
+X15:9500,
+X16:10700,
+X17:8600,
+X18:9900,
+X19:9300,
+X20:10800,
+X21:9600,
+X22:10400,
+X23:8500
+};
+
 function cariVariabel(item){
   let tanggal = item.hariTanggal.split(", ")[1];
   return mappingVariabel[tanggal];
@@ -296,9 +348,13 @@ function cariVariabel(item){
 
 /* update otomatis nilai gizi */
 menus.forEach(item => {
-  let x = cariVariabel(item);
-  item.giziKecil = giziKecilMap[x];
-  item.giziBesar = giziBesarMap[x];
+let x = cariVariabel(item);
+
+item.hargaKecil = hargaKecilMap[x];
+item.hargaBesar = hargaBesarMap[x];
+
+item.giziKecil = giziKecilMap[x];
+item.giziBesar = giziBesarMap[x];
 });
 
 console.log(menus);
@@ -353,7 +409,6 @@ let kategori = document.getElementById("kategori").value;
 
 let sortedMenus = [...menus];
 
-/* hitung rasio */
 if(kategori === "kecil"){
 
   sortedMenus.forEach(item=>{
@@ -368,10 +423,8 @@ if(kategori === "kecil"){
 
 }
 
-/* urutkan dari rasio terbesar */
 sortedMenus.sort((a,b)=>b.rasio-a.rasio);
 
-/* ambil 6 menu terbaik */
 sortedMenus = sortedMenus.slice(0,6);
 
 let hasil = "<h3>Daftar 6 Menu Optimal</h3><ul>";
